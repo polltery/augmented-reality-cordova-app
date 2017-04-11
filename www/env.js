911141569b6d78fcacca22c29f63b54606d55390
Balraj <polltery@gmail.com>
2017-04-11 18:54:49 +0400

    Update many things
    - Add env.js to specify important parameters for the application
    - Update AR interface
    - Seperate AR logic to interactions.js
    - Update index UI

---
+++ www/env.js
@@ -0,0 +1,3 @@
+window.ENV = {
+    apiUrl : "http://192.168.1.5:3000"
+};
---
+++ www/js/interactions.js
@@ -0,0 +1,98 @@
+// Form methods : on click of toggle button send a post request
+$(document).on("click", "#switchBtn", function (e) {
+
+    e.preventDefault();
+
+    $.ajax({
+        type: 'GET',
+        url: apiUrl + '/toggle',
+        success: function (data) {
+            //
+        },
+        error: function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            alert(msg);
+        }
+    });
+});
+
+// Form methods : on click of toggle red button, send a get request
+$(document).on("click", "#toggleRedBtn", function (e) {
+
+    e.preventDefault();
+
+    $.ajax({
+        type: 'GET',
+        url: apiUrl + '/toggle/led',
+        success: function (data) {
+            //
+        },
+        error: function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            alert(msg);
+        }
+    });
+});
+
+// Form methods : on click of toggle red button
+$(document).on("click", "#toggleGreenBtn", function (e) {
+
+    e.preventDefault();
+
+    $.ajax({
+        type: 'GET',
+        url: apiUrl + '/toggle/led2',
+        success: function (data) {
+            //
+        },
+        error: function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            alert(msg);
+        }
+    });
+});
