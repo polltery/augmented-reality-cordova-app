e22a1fe93b9e2d0a51fd9096934a6274b1b25eb6
Balraj <polltery@gmail.com>
2017-04-13 23:45:00 +0400

    Add ability for unique interactions

--- www/js/commons.js
+++ www/js/commons.js
@@ -2,7 +2,48 @@ var popUpActive = false;
 var apiUrl = localStorage.getItem('apiUrl');
 var craftARcollectionToken = localStorage.getItem('craftARCollectionToken');
 
-// Inject html into popUpDiv
+// Inject html into popUpDiv, this is the default popup
 $(function(){
-    $("#popUpDiv").load("views/popup.html"); 
+    $("#popUpDiv").load("views/defaultInterface.html"); 
 });
+
+// Get details of recognised items
+function processResults(results){
+    var item = results[0].item;
+    itemHTML = item;
+    
+    // Get item info
+    $("#nameResponse").html(item.name);
+    $("#image_src").attr("src", results[0].matchedImage.thumbnail320Url);
+    $("#idResponse").html(item.uuid);
+
+    var ARInterface = "";
+
+    // Set appropriate UI
+    switch (item.uuid) {
+        // Switch 2
+        case "0239c52fe2b3455ab8513e263f5a3156":
+            ARInterface = "views/switchTwoInterface.html";
+            break;
+        // Switch 1
+        case "75fcf1ae693a499aa60207cb1af74bf1":
+            ARInterface = "views/switchInterface.html";
+            break;
+        // Fan
+        case "3a134e274a2a43069fd841df92260ed0":
+            ARInterface = "views/fanInterface.html";
+            break;
+        default:
+            break;
+    }
+
+    if(ARInterface !== ""){
+        $("#popUpDiv").load(ARInterface);
+    }
+
+    // Show UI
+    $('#popUpDiv').fadeIn("fast", function () {
+        popUpActive = true;
+    });
+
+}
