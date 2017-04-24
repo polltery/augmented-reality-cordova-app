b0ea91206cfdbf849f644d14d53a0db94b12023b
Balraj <polltery@gmail.com>
2017-04-24 06:59:26 +0400

    Add comments

--- www/env.js
+++ www/env.js
@@ -1,4 +1,6 @@
 window.ENV = {
-    apiUrl : "http://192.168.1.5:3000",
+    // URL of the accessible API
+    apiUrl : "http://10.11.14.66:3000",
+    // Collection token from craftAR
     craftARCollectionToken : "08c9ac829f644bcc"
 };
